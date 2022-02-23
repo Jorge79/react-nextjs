c5791b774420a29e495e17d2f3f3baa47dad2d9a
Jorge Francisco <Jorge7.9@hotmail.com>
2022-02-23 20:21:01 -0300

    feat: Add faq styles and trailing route

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,16 @@
-import Head from 'next/head';
-import Link from '../src/components/Link';
+// import Head from 'next/head'
+// import Link from '../src/components/Link';
+import FAQScreen from '../src/screens/FAQScreen';
+export default FAQScreen;
 
+// SSG - Static Site Generation
+// SSR - Server Side Rendering
+// ISG - Incremental Static Generation
+
+// export async function getServerSideProps() {
+//     console.log('Em modo DEV, sempre roda! A cada acesso')
+//     console.log('Rodando a cada acesso que você recebe')
 export async function getStaticProps() {
-  console.log('rodando no build');
   const FAQ_API_URL =
     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
   const faq = await fetch(FAQ_API_URL)
@@ -12,32 +20,36 @@ export async function getStaticProps() {
     .then((resposta) => {
       return resposta;
     });
+
   return {
     props: {
-      qualquercoisa: 'teste',
+      qualquercoisa: 'que eu passar aqui',
       faq,
-    }, // will be passed to the page component as props
+    },
   };
 }
 
-export default function Faq({ faq }) {
-  return (
-    <div>
-      <Head>
-        <title>FAQ - Alura Cases Campanha</title>
-      </Head>
-      <h1>Alura Cases - Faq</h1>
-      <Link href='/'>Ir para a Home</Link>
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li key={question}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+// export default function FAQPage({ faq }) {
+//     console.log('Isso roda no servidor???')
+//     return (
+//         <div>
+//             <Head>
+//                 <title>FAQ - Alura Cases Campanha</title>
+//             </Head>
+//             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+//             <Link href="/">
+//                 Ir para a home
+//             </Link>
+//             <ul>
+//                 {faq.map(({ answer, question }) => (
+//                     <li key={question}>
+//                         <article>
+//                             <h2>{question}</h2>
+//                             <p>{answer}</p>
+//                         </article>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
